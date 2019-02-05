e6015009a33045032c9a2f4a1849c81b86fa8a62
Benevolent12 <38099136+Benevolent12@users.noreply.github.com>
2019-02-05 20:00:00 +0200

    message 1

--- src/Exception/Main.java
+++ src/Exception/Main.java
@@ -5,12 +5,14 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        Exception ex = new Exception("sdfdsf");
-        
+        /*
+        //Exception ex = new Exception("sdfdsf");
+         
          //System.out.println( args[0] );
         try
         {
-            throw ex;
+         //   throw ex;
+            throw new Exception("sdfdsf dfgdfg");
         //    String AA=args[0];
        //     System.out.println( AA );
         }
@@ -19,6 +21,46 @@ public class Main
             System.out.println( exp.getMessage() );
         //    System.out.println( exp.toString() );
         }
-        
+        */
+        /*
+        try
+        {
+            MyClass mc = new MyClass();
+            mc.method1();
+        }
+        catch(Exception exp)
+        {
+           exp.printStackTrace();
+        }
+        */
+        method1();
     }
+    public static void method1()
+    {
+        method2();
+    }
+    public static void method2()
+    {
+        method3();
+    }
+    public static void method3()
+    {
+        StackTraceElement[] arr1=Thread.currentThread().getStackTrace();
+        for (int i = 0; i < arr1.length; i++) 
+        {
+            System.out.println( arr1[i].getMethodName() ); 
+        }
+    }
+}
+class MyClass
+{
+    public void method1() throws Exception
+    {
+        Exception ex = new Exception("sdfdsf"); throw ex;
+    }
+    
+}
+class StackTR
+{
+    
 }
